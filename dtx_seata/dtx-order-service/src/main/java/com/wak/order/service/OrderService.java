0450cbbbe6833d8e8da4d83b3c3c72955192d72d
WAKABC <wuankang588@163.com>
2023-12-21 11:21:17 +0800

    消除部分警告

--- dtx_seata/dtx-order-service/src/main/java/com/wak/order/service/OrderService.java
+++ dtx_seata/dtx-order-service/src/main/java/com/wak/order/service/OrderService.java
@@ -3,13 +3,12 @@ package com.wak.order.service;
 import com.wak.commons.apis.AccountFeignApi;
 import com.wak.commons.apis.StorageFeignApi;
 import com.wak.commons.entities.Order;
-import io.seata.common.util.StringUtils;
+import com.wak.order.mapper.OrderMapper;
 import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import com.wak.order.mapper.OrderMapper;
 
 /**
  * @author wuankang
@@ -38,7 +37,7 @@ public class OrderService {
      * @GlobalTransactional + @Transactional = SEATA.XA/AT
      */
     /*@GlobalTransactional + @Transactional(rollbackFor = Exception.class) XA*/
-    //@GlobalTransactional(name = "seata-order-server", rollbackFor = Exception.class) AT
+    //@GlobalTransactional(name = "seata-order-server", rollbackFor = Exception.class) AT/TCC
     @GlobalTransactional(name = "order-server", rollbackFor = Exception.class)
     public Order create(Order order) {
         //全局事务id
@@ -46,7 +45,9 @@ public class OrderService {
         //新建订单
         order.setStatus(0);
         int count = orderMapper.insertSelective(order);
-        if (count <= 0) throw new RuntimeException("新建订单失败...");
+        if (count <= 0){
+            throw new RuntimeException("新建订单失败...");
+        }
         log.info("******订单创建完成****** \n order:{}", order);
         //扣库存
         storageFeignApi.decreaseTcc(order.getProductId(), order.getCount());
@@ -56,7 +57,9 @@ public class OrderService {
         log.info("******扣减金额成功*******");
         //修改订单状态
         int updateResult = orderMapper.updateStatusById(1, order.getId());
-        if (updateResult > 0) log.info("******更新订单状态成功*******");
+        if (updateResult > 0){
+            log.info("******更新订单状态成功*******");
+        }
         log.info("********订单创建完成**********\n xid:{}", xid);
         return order;
     }
